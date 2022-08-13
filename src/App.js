de8c6310147712e75bc14c7ce8d5fab9d4fc82bf
Alicia <arrowsrp@yahoo.com>
2022-08-13 14:41:37 -0700

    Removes fetch request from app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import logo from './logo.svg';
-import { useEffect, useState } from 'react';
 import DataContainer from './DataContainer';
 import './App.css';
 
 function App() {
-  const [data, setData]=useState([])
-  useEffect(()=>{
-    fetch("https://www.ebi.ac.uk/cgi-bin/ipd/api/allele?limit=1000")
-    .then((r)=> r.json())
-    .then((data)=>console.log(data));
-  }, []);
+  
   
   return (
     <div className="App">
